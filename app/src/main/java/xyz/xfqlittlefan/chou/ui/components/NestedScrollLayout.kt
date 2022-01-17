603ad69cd73ef2f8f7c1c90fea501bbf0157bc2d
xfqwdsj <xfqwdsj@qq.com>
2022-01-17 13:40:47 +0800

    测试 NestedScroll

--- app/src/main/java/xyz/xfqlittlefan/chou/ui/components/NestedScrollLayout.kt
+++ app/src/main/java/xyz/xfqlittlefan/chou/ui/components/NestedScrollLayout.kt
@@ -106,14 +106,17 @@ class NestedScrollBehavior(private val coroutineScope: CoroutineScope) {
                     contentOffset = 0f
                 }
             }
-            if (contentOffset == 0f) blankOffset = (blankOffset + available.y).coerceIn(minimumValue = offsetLimit, maximumValue = 0f)
+            if (available.y < 0) blankOffset = (blankOffset + available.y).coerceIn(minimumValue = offsetLimit, maximumValue = 0f)
             Log.w("!!!Chou!!!", "contentOffset=$contentOffset\nblankOffset=$blankOffset\nnewOffset=$offset\nresult=$difference")
             return Offset(x = 0f, y = if (blankOffset > offsetLimit) difference else 0f)
         }
 
         override fun onPostScroll(consumed: Offset, available: Offset, source: NestedScrollSource): Offset {
             contentOffset += consumed.y
-            return Offset.Zero
+            val newOffset = (blankOffset + available.y).coerceIn(minimumValue = offsetLimit, maximumValue = 0f)
+            val difference = newOffset - blankOffset
+            if (available.y > 0) blankOffset = newOffset
+            return Offset(x = 0f, y = difference)
         }
 
         override suspend fun onPreFling(available: Velocity): Velocity {
